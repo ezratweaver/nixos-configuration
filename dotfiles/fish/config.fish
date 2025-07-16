d0dc9f563edd1a7226a428fb59653ece13d9e8f1
ezratweaver <ezratweaver@gmail.com>
2025-07-16 13:14:41 -0400

    Move fish variables to config

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -88,3 +88,34 @@ end
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
 set -gx PATH $PATH /home/ezratweaver/go/bin
+
+# Fish color scheme
+set -U fish_color_autosuggestion brblack
+set -U fish_color_cancel \-r
+set -U fish_color_command blue
+set -U fish_color_comment red
+set -U fish_color_cwd green
+set -U fish_color_cwd_root red
+set -U fish_color_end green
+set -U fish_color_error brred
+set -U fish_color_escape brcyan
+set -U fish_color_history_current \-\-bold
+set -U fish_color_host normal
+set -U fish_color_host_remote yellow
+set -U fish_color_normal normal
+set -U fish_color_operator brcyan
+set -U fish_color_param cyan
+set -U fish_color_quote yellow
+set -U fish_color_redirection cyan\e\-\-bold
+set -U fish_color_search_match bryellow\e\-\-background\=brblack
+set -U fish_color_selection white\e\-\-bold\e\-\-background\=brblack
+set -U fish_color_status red
+set -U fish_color_user brgreen
+set -U fish_color_valid_path \-\-underline
+
+# Pager colors
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description yellow\e\-i
+set -U fish_pager_color_prefix normal\e\-\-bold\e\-\-underline
+set -U fish_pager_color_progress brwhite\e\-\-background\=cyan
+set -U fish_pager_color_selected_background \-r
