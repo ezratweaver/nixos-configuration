194ea78d5376b2a96f0359039e9d9a8d8b8da846
ezratweaver <ezratweaver@gmail.com>
2025-07-16 11:51:46 -0400

    Update config to work

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Ezra's NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -19,6 +19,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";
             home-manager.users.ezratweaver = import ./home.nix;
           }
         ];
