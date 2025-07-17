13b1d1bf17f45a58a67cc94bcb2eef50dc34cf87
ezratweaver <ezratweaver@gmail.com>
2025-07-17 15:58:56 -0400

    Fix tab complete

--- dotfiles/nvim/lua/config/lazy.lua
+++ dotfiles/nvim/lua/config/lazy.lua
@@ -15,13 +15,10 @@ require("lazy").setup({
     { import = "plugins" },
     {
       "hrsh7th/nvim-cmp",
-      dependencies = {
-        "hrsh7th/cmp-emoji",
-      },
       ---@param opts cmp.ConfigSchema
       opts = function(_, opts)
-        unpack = unpack or table.unpack
         local has_words_before = function()
+          unpack = unpack or table.unpack
           local line, col = unpack(vim.api.nvim_win_get_cursor(0))
           return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
         end
@@ -31,6 +28,7 @@ require("lazy").setup({
         opts.mapping = vim.tbl_extend("force", opts.mapping, {
           ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
+              -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
               cmp.select_next_item()
             elseif vim.snippet.active({ direction = 1 }) then
               vim.schedule(function()
