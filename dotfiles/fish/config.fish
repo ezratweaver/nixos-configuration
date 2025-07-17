f7a82f726c895ab581d3fb1c66c4fbbe224ddd97
ezratweaver <ezratweaver@gmail.com>
2025-07-17 14:45:23 -0400

    Change color scheme to match terminal

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -29,11 +29,6 @@ if status is-interactive
         sudo nixos-rebuild switch --flake /etc/nixos#$NIX_HOST
     end
 
-    function hmreb
-        source /etc/nixos/.env
-        home-manager switch --flake /etc/nixos#$NIX_HOST
-    end
-
     function nxupdate
         cd /etc/nixos
         source .env
--- dotfiles/nvim/lua/plugins/colorscheme.lua
+++ dotfiles/nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,50 @@
 return {
-  { "AlexvZyl/nordic.nvim", "navarasu/onedark.nvim", "rebelot/kanagawa.nvim", "Mofiqul/vscode.nvim" },
-  { "LazyVim/LazyVim", opts = { colorscheme = "onedark" } },
+  {
+    "rebelot/kanagawa.nvim",
+    opts = {
+      transparent = true,
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              bg = "#191919",
+              fg = "#BBBBBB",
+              fg_dim = "#8E8E8E",
+            },
+            syn = {
+              string = "#8BAE68",
+              number = "#D68C67",
+              constant = "#61ABDA",
+              identifier = "#8E8E8E",
+              statement = "#CF86C1",
+              fun = "#65B8C1",
+              operator = "#E8838F",
+              keyword = "#DE6E7C",
+              type = "#B279A7",
+              regex = "#819B69",
+              deprecated = "#8E8E8E",
+            },
+            diff = {
+              add = "#8BAE68",
+              delete = "#E8838F",
+              change = "#D68C67",
+              text = "#61ABDA",
+            },
+            diag = {
+              error = "#E8838F",
+              warn = "#D68C67",
+              info = "#61ABDA",
+              hint = "#65B8C1",
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa-dragon",
+    },
+  },
 }
