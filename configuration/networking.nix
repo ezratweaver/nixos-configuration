b1e9c1ac9518a27ee24de5f40c292cea06213c34
ezratweaver <ezratweaver@gmail.com>
2025-08-09 19:45:34 -0400

    Disable ipv6

--- configuration/networking.nix
+++ configuration/networking.nix
@@ -1,10 +1,16 @@
 {
-  networking.hostName = "ezratweaver";
+  networking.hostName = "linux-11807cc6-f4f7-455c-8831-3a7d5cccb104";
 
+  # IWD for netowkring
   networking.wireless.iwd.enable = true;
-  
+
+  # Disable IPv6, because most vpns don't support it out of the box.
+  networking.enableIPv6 = false;
+ 
+  # Enable bluetooth
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
-  
-  services.openssh.enable = true;
+ 
+  # Enable SSH
+  services.openssh.enable = true; 
 }
