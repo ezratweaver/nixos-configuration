7e134e62ff6d82e6f2b24693dc7871911f4b2fcf
ezratweaver <ezratweaver@gmail.com>
2025-08-11 15:01:42 -0400

    Add mac address randomizing and cloudflared dns over https

--- modules/networking.nix
+++ modules/networking.nix
@@ -4,6 +4,15 @@
   # IWD for netowkring
   networking.wireless.iwd.enable = true;
 
+  # Randomize mac address
+  environment.etc."iwd/main.conf".text = ''
+    [General]
+    EnableNetworkConfiguration=true
+
+    [Privacy]
+    EnableAddressRandomization=true
+  '';
+
   # Disable IPv6, because most vpns don't support it out of the box.
   networking.enableIPv6 = false;
  
@@ -13,4 +22,18 @@
  
   # Enable SSH
   services.openssh.enable = true; 
+
+  # Enable cloudflared as a local DNS-over-HTTPS proxy (avoid dns query spying)
+  services.cloudflared = {
+    enable = true;
+    config = ''
+      proxy-dns: true
+      proxy-dns-port: 5053
+      proxy-dns-upstream:
+        - https://mozilla.cloudflare-dns.com/dns-query
+    '';
+  };
+
+  # Set system DNS to point to local cloudflared proxy
+  networking.nameservers = [ "127.0.0.1" ];
 }
