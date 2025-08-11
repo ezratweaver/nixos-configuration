ae0612d5079f4d55dc1e2277e67c95b82c2365a8
ezratweaver <ezratweaver@gmail.com>
2025-08-11 09:59:07 -0400

    Remove duplicate config stuff

--- modules/system/desktop/hyprland.nix
+++ modules/system/desktop/hyprland.nix
@@ -1,11 +1,6 @@
-{ pkgs, ... }:
-
 {
   # Enable touchpad
   services.libinput.enable = true;
-  
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   
   # Desktop environment
   programs.hyprland.enable = true;
