4e3bdf9aa27497d862f0f5efe54f36657401a2e1
ezratweaver <ezratweaver@gmail.com>
2025-08-11 09:56:47 -0400

    Move host config stuff out of hardware config file

--- hosts/work-laptop/default.nix
+++ hosts/work-laptop/default.nix
@@ -1,7 +1,16 @@
+{ pkgs, ... }:
+
 {
   imports = [
     ./hardware.nix
     ../../shared
+  ];
+
+  # Work specific packages
+  environment.systemPackages = with pkgs; [
+    teams-for-linux
+    awscli2
+    s5cmd
   ];
 
   system.stateVersion = "25.05";
