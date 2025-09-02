190ec0a6738110b71abf5b5fb367d413f93a5a38
ezratweaver <ezratweaver@gmail.com>
2025-09-02 08:13:39 -0400

    Update fish prompt to include git branch and exit status

--- home/dotfiles/fish/config.fish
+++ home/dotfiles/fish/config.fish
@@ -99,6 +99,9 @@ if status is-interactive
     end
 
     function fish_prompt
+        # Capture last exit status
+        set -l last_status $status
+
         # Check if we're in a nix shell or nix develop environment
         set -l nix_indicator ""
         if test -n "$IN_NIX_SHELL"
@@ -111,10 +114,27 @@ if status is-interactive
             set nix_indicator (set_color magenta)"[nix-develop]"(set_color normal)" "
         end
 
-        # Basic prompt with nix indicator
+        # Git branch
+        set -l git_branch ""
+        if type -q git
+            set branch (git rev-parse --abbrev-ref HEAD 2>/dev/null)
+            if test $status -eq 0
+                set git_branch " "(set_color yellow)"($branch)"(set_color normal)
+            end
+        end
+
+        # Exit status indicator
+        set -l status_indicator ""
+        if test $last_status -ne 0
+            set status_indicator " "(set_color red)"[$last_status]"(set_color normal)
+        end
+
+        # Build prompt
         echo -n $nix_indicator
         echo -n (set_color white)(whoami)(set_color normal)" "
         echo -n (set_color $fish_color_cwd)(prompt_pwd)(set_color normal)
+        echo -n $git_branch
+        echo -n $status_indicator
         echo -n " \$ "
     end
 
