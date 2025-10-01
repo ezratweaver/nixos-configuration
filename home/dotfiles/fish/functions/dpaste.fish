58c59be6595a996679f96a3e6bdff8ef5869c64d
ezratweaver <ezratweaver@gmail.com>
2025-10-01 14:58:40 -0400

    Add optional arguement to dpaste

--- home/dotfiles/fish/functions/dpaste.fish
+++ home/dotfiles/fish/functions/dpaste.fish
@@ -1,12 +1,18 @@
 function dpaste
+    set -l dest .
+    if test (count $argv) -gt 0
+        set dest $argv[1]
+    end
+
     set -l clip (wl-paste)
 
     if string match -q "CUT:*" $clip
         set -l dir (string replace "CUT:" "" $clip)
-        mv $dir .
-        echo "Moved: $dir -> (pwd)"
+        mv $dir $dest
+        echo "Moved: $dir -> $dest"
     else
-        cp -r $clip .
-        echo "Copied: $clip -> (pwd)"
+        cp -r $clip $dest
+        echo "Copied: $clip -> $dest"
     end
 end
+
