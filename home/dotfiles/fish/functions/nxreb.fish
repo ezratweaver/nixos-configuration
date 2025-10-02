dd5fa8a690d6b4a97f535e2bd4b872ee29c782b1
ezratweaver <ezratweaver@gmail.com>
2025-10-01 22:31:22 -0400

    Revert "Add profile name to rebuilds"
    This reverts commit cb8f351d5317db51f73ad24681f0bec7bc79b328.

--- home/dotfiles/fish/functions/nxreb.fish
+++ home/dotfiles/fish/functions/nxreb.fish
@@ -1,12 +1,4 @@
 function nxreb
     source ~/nixos-configuration/.env
-
-    set shortRev (git -C ~/nixos-configuration rev-parse --short HEAD 2>/dev/null; or echo dirty)
-    set commitMsg (git -C ~/nixos-configuration log -1 --pretty=%s 2>/dev/null; or echo "local build")
-    set buildTime (date "+%Y-%m-%d %H:%M")
-
-    set profileName "[$shortRev] $commitMsg ($buildTime)"
-
-    sudo nixos-rebuild switch --flake ~/nixos-configuration/#$NIX_HOST --profile-name "$profileName"
+    sudo nixos-rebuild switch --flake ~/nixos-configuration/#$NIX_HOST
 end
-
