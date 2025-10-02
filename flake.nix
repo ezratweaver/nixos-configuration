9d26d1bb8df33bef3734730477433eaa868d4205
ezratweaver <ezratweaver@gmail.com>
2025-10-01 22:23:23 -0400

    Add configuration for custom grub configuration names

--- flake.nix
+++ flake.nix
@@ -18,6 +18,7 @@
 
   outputs =
     {
+      self,
       nixpkgs-unstable,
       nixpkgs-2505,
       nix-ai-tools,
@@ -72,6 +73,32 @@
             home-manager.nixosModules.home-manager
             homeManagerConfig
             nixpkgsConfig
+
+            (
+              { lib, ... }:
+              let
+                rev = self.rev or "dirty";
+
+                shortRev = builtins.substring 0 7 rev;
+
+                commitMsg =
+                  builtins.tryEval
+                    (builtins.readFile (
+                      builtins.toString (
+                        nixpkgs.runCommand "git-subject" { buildInputs = [ nixpkgs.git ]; } ''
+                          cd ${toString ./.}
+                          git log -1 --pretty=%s > $out
+                        ''
+                      )
+                    )).value or "local build";
+
+                buildTime = lib.formatTime "%Y-%m-%d %H:%M" builtins.currentTime;
+              in
+              {
+                boot.loader.grub.configurationName = "[${shortRev}] ${commitMsg} (${buildTime})";
+              }
+            )
+
           ];
         };
     in
