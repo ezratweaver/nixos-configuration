5fb7032c21546452f2c952f1d5df2a5af15d2f33
ezratweaver <ezratweaver@gmail.com>
2025-11-13 10:42:01 -0500

    Add personal firefox settings

--- home/modules/firefox.nix
+++ home/modules/firefox.nix
@@ -192,6 +192,17 @@
         "browser.menu.showViewImageInfo" = true;
         "findbar.highlightAll" = true;
         "layout.word_select.eat_space_to_next_word" = false;
+
+        # Personal preferences
+        "browser.fullscreen.autohide" = false;
+        "sidebar.verticalTabs" = true;
+        "browser.tabs.tabmanager.enabled" = false;
+        "ui.systemUsesDarkTheme" = 1;
+        "browser.toolbars.bookmarks.visibility" = "always";
+        "extensions.formautofill.addresses.enabled" = false;
+        "extensions.formautofill.creditCards.enabled" = false;
+        "signon.rememberSignons" = false;
+        "ui.key.menuAccessKeyFocuses" = false;
       };
     };
   };
