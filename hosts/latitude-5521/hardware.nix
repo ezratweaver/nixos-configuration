300b3803fe09df5130dd2eb426863c19ccb0d4a4
ezratweaver <ezratweaver@gmail.com>
2025-12-09 22:02:25 -0500

    Add more hardware fixes

--- hosts/latitude-5521/hardware.nix
+++ hosts/latitude-5521/hardware.nix
@@ -50,4 +50,10 @@
 
   # Force S3 sleep mode to preserve battery during sleep (idk what that means)
   boot.kernelParams = [ "mem_sleep_default=deep" ];
+
+  # Fix for sdcard reader
+  boot.blacklistedKernelModules = [ "firewire_ohci" ];
+
+  # Fix Intel cpu throttling
+  services.throttled.enable = true;
 }
