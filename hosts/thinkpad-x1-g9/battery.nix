13aef4f0672508982299cc1346aae83fdf538ac5
ezratweaver <ezratweaver@gmail.com>
2025-12-16 17:06:32 -0500

    Add more configurations to tlp

--- hosts/thinkpad-x1-g9/battery.nix
+++ hosts/thinkpad-x1-g9/battery.nix
@@ -4,8 +4,14 @@
   services.tlp = {
     enable = true;
     settings = {
+      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_BAT = "low-power";
+
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
 
       START_CHARGE_THRESH_BAT0 = 90;
       STOP_CHARGE_THRESH_BAT0 = 98;
@@ -16,6 +22,12 @@
       # Enable Audio Power Saving
       SOUND_POWER_SAVE_ON_AC = 0;
       SOUND_POWER_SAVE_ON_BAT = 1;
+
+      PCIE_ASPM_ON_AC = "performance";
+      PCIE_ASPM_ON_BAT = "powersave";
+
+      NVME_PIN_STEERING_ON_AC = "performance";
+      NVME_PIN_STEERING_ON_BAT = "powersave";
     };
   };
 }
