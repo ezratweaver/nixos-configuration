dd0c9a12fd4499cbae170e1eb7ae7cdbc22ce8fe
ezratweaver <ezratweaver@gmail.com>
2025-12-20 00:49:38 -0500

    Switch to grub for bootloader

--- system/core/boot.nix
+++ system/core/boot.nix
@@ -1,6 +1,22 @@
 {
   # Boot configuration
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-}
+  boot.loader = {
+    # Disable systemd-boot
+    systemd-boot.enable = false;
 
+    # Enable GRUB
+    grub = {
+      enable = true;
+      # "nodev" is standard for UEFI systems
+      device = "nodev";
+      efiSupport = true;
+      # This helps avoid some EFI variable issues
+      efiInstallAsRemovable = true;
+    };
+
+    # Ensure efi variables can be touched if needed
+    # Note: If efiInstallAsRemovable is true, some users set this to false
+    # to avoid the exact "assertion failed" bug you are seeing.
+    efi.canTouchEfiVariables = false;
+  };
+}
