cae92b7c0c662a495ee7b1d1669f8d18611e8c6b
ezratweaver <ezratweaver@gmail.com>
2025-12-20 01:16:51 -0500

    Add boot splash screen

--- system/core/boot.nix
+++ system/core/boot.nix
@@ -1,3 +1,5 @@
+{ pkgs, ... }:
+
 {
   # Boot configuration
   boot.loader = {
@@ -7,16 +9,38 @@
     # Enable GRUB
     grub = {
       enable = true;
-      # "nodev" is standard for UEFI systems
       device = "nodev";
       efiSupport = true;
-      # This helps avoid some EFI variable issues
       efiInstallAsRemovable = true;
+
+      theme = pkgs.minimal-grub-theme;
     };
 
-    # Ensure efi variables can be touched if needed
-    # Note: If efiInstallAsRemovable is true, some users set this to false
-    # to avoid the exact "assertion failed" bug you are seeing.
     efi.canTouchEfiVariables = false;
   };
+
+  boot.plymouth = {
+    enable = true;
+
+    theme = "hexagon_dots";
+    themePackages = with pkgs; [
+      # By default we would install all themes
+      (adi1090x-plymouth-themes.override {
+        selected_themes = [ "hexagon_dots" ];
+      })
+    ];
+  };
+
+  boot.consoleLogLevel = 0;
+  boot.initrd.verbose = false;
+
+  boot.kernelParams = [
+    "quiet"
+    "splash"
+    "boot.shell_on_fail"
+    "loglevel=3"
+    "rd.systemd.show_status=false"
+    "rd.udev.log_level=3"
+    "udev.log_priority=3"
+  ];
 }
