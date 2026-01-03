052c4697d2bf4ce3b7d1afc0b337d72864d3b55f
ezratweaver <ezratweaver@gmail.com>
2026-01-03 17:35:20 -0500

    Add policy file for helium

--- system/desktop/browsers.nix
+++ system/desktop/browsers.nix
@@ -7,6 +7,53 @@
     helium
   ];
 
+  environment.etc."/chromium/policies/managed/policy.json".source =
+    (pkgs.formats.json { }).generate "policy.json"
+      {
+        AutofillEnabled = false;
+        AutofillProfileEnabled = false;
+        AutofillCreditCardEnabled = false;
+        PasswordManagerEnabled = false;
+        MetricsReportingEnabled = false;
+        CrashReportingEnabled = false;
+        BackgroundModeEnabled = false;
+        DnsOverHttpsMode = "secure";
+        PromotionalTabsEnabled = false;
+        PrivacySandboxPromptEnabled = false;
+
+        DefaultSearchProviderEnabled = true;
+        DefaultSearchProviderSearchURL = "https://startpage.com/sp/search?q={searchTerms}";
+        DefaultSearchProviderName = "StartPage";
+        DefaultSearchProviderKeyword = "startpage.com";
+
+        ExtensionInstallForcelist = map (extension: extension.id) [
+          {
+            name = "Bitwarden";
+            id = "nngceckbapebfimnlniiiahkandclblb";
+          }
+          {
+            name = "Vimium";
+            id = "dbepggeogbaibhgnhhndojpepiihcmeb";
+          }
+          {
+            name = "SponserBlock for Youtube";
+            id = "mnjggcdmjocbbbhaepdhchncahnbgone";
+          }
+          {
+            name = "Uninternet";
+            id = "ihgeijoonjmdfkamhpgoedplnmbencgd";
+          }
+          {
+            name = "Remove Youtube Shorts";
+            id = "mgngbgbhliflggkamjnpdmegbkidiapm";
+          }
+          {
+            name = "Dark Theme for Google Chrome";
+            id = "annfbnbieaamhaimclajlajpijgkdblo";
+          }
+        ];
+      };
+
   environment.etc."/brave/policies/managed/GroupPolicy.json".source =
     (pkgs.formats.json { }).generate "brave-managed-policy.json"
       {
