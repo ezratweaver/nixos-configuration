5df1ea602724dc6fd7210e47037ba3b5d173a7cc
ezratweaver <ezratweaver@gmail.com>
2026-01-03 22:51:51 -0500

    Remove commented out code

--- hosts/thinkpad-x1-g9/battery.nix
+++ hosts/thinkpad-x1-g9/battery.nix
@@ -1,33 +1,6 @@
 { ... }:
 
 {
-  # services.tlp = {
-  #   enable = true;
-  #   settings = {
-  #     PLATFORM_PROFILE_ON_AC = "performance";
-  #     PLATFORM_PROFILE_ON_BAT = "low-power";
-  #
-  #     CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-  #     CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
-  #
-  #     START_CHARGE_THRESH_BAT0 = 90;
-  #     STOP_CHARGE_THRESH_BAT0 = 98;
-  #
-  #     WIFI_PWR_ON_AC = "off";
-  #     WIFI_PWR_ON_BAT = "on";
-  #
-  #     # Enable Audio Power Saving
-  #     SOUND_POWER_SAVE_ON_AC = 0;
-  #     SOUND_POWER_SAVE_ON_BAT = 1;
-  #
-  #     PCIE_ASPM_ON_AC = "performance";
-  #     PCIE_ASPM_ON_BAT = "powersave";
-  #
-  #     NVME_PIN_STEERING_ON_AC = "performance";
-  #     NVME_PIN_STEERING_ON_BAT = "powersave";
-  #   };
-  # };
-
   services.undervolt = {
     enable = true;
     p1 = {
