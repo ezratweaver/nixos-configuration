77d3c092d1333d48bb103705fdd81033670c1e2b
ezratweaver <ezratweaver@gmail.com>
2026-01-05 13:25:20 +0000

    Add timezone fixes

--- configuration.nix
+++ configuration.nix
@@ -1,9 +1,15 @@
+{ lib, ... }:
+
 {
   imports = [
     ./system/core
     ./system/desktop
     ./system/packages.nix
   ];
+
+  services.geoclue2.enable = true;
+
+  time.timeZone = lib.mkDefault "America/New_York";
 
   services.automatic-timezoned.enable = true;
 }
