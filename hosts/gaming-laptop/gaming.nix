4ce860bd92d88cd074d0c33589a6e8bb814c0451
ezratweaver <ezratweaver@gmail.com>
2026-02-27 19:26:16 +0000

    Add wine dependecies

--- hosts/gaming-laptop/gaming.nix
+++ hosts/gaming-laptop/gaming.nix
@@ -5,16 +5,21 @@
     lutris
     heroic
     ryubing
+    umu-launcher
 
     rpcs3
 
     alvr # Stream to vr
+
+    wine
+    winetricks
+    protonup-qt
   ];
 
   programs.steam = {
     enable = true;
     gamescopeSession.enable = true;
-    extraCompatPackages = [ pkgs.proton-ge-bin];
+    extraCompatPackages = [ pkgs.proton-ge-bin ];
   };
 
   # Workarounds for XBOX controller (bluetooth)
@@ -32,6 +37,17 @@
     };
   };
 
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [
+      vulkan-loader
+      vulkan-validation-layers
+      vulkan-extension-layer
+    ];
+  };
+
+  programs.gamemode.enable = true;
 
   boot = {
     extraModulePackages = with config.boot.kernelPackages; [ xpadneo ];
