120562927758dfa28a871db335a641eee8fc79f0
ezratweaver <ezratweaver@gmail.com>
2026-04-01 09:09:10 -0400

    fix(zenbook): add bluetooth tweak

--- hosts/asus-zenbook/tweaks.nix
+++ hosts/asus-zenbook/tweaks.nix
@@ -9,4 +9,9 @@
 
   # better power management
   services.power-profiles-daemon.enable = true;
+
+  # hack to fix occasional choppy bluetooth
+  boot.extraModprobeConfig = ''
+    options iwlwifi bt_coex_active=0
+  '';
 }
